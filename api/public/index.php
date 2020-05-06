94e50333d866a4a4404862cfc0be3e864b78facf
shyrops <beee.semok@gmail.com>
2020-05-06 19:51:41 +0300

    1. Clear index.php 2. Add configuration files

--- api/public/index.php
+++ api/public/index.php
@@ -2,29 +2,18 @@
 
 declare(strict_types=1);
 
-use App\Http\Action\HomeAction;
-use DI\ContainerBuilder;
-use Slim\Factory\AppFactory;
+use Psr\Container\ContainerInterface;
+use Slim\App;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new ContainerBuilder();
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
 
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG'),
-    ]
-]);
-
-$container = $builder->build();
-
-$app = AppFactory::createFromContainer($container);
-
-$app->addErrorMiddleware($container->get('config')['debug'], true, true);
-
-$app->get('/', HomeAction::class);
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 
 $app->run();
 
